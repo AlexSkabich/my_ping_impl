315a6e196f84f44ef5eef3b931d86a192309f544
AlexSkabich <skabichevsky.alex@gmail.com>
2026-07-05 20:01:20 +0500

    Finished icmp implementation

--- ping.c
+++ ping.c
@@ -7,5 +7,27 @@
 
 int main()
 {
+  int8_t *str, *raw;
+  icmp_t *pkt;
+
+  str = (int8_t*)malloc(13);
+  if(!str) exit(1);
+
+  memset(str, 0, 13);
+  memcpy(str, "Hello, World", 12);
+
+  pkt = mkicmp(echo, str, 13);
+  if(!pkt) {printf("Pkt error: 19\n"); exit(1);}
+
+  showicmp(pkt);
+
+  //s_size = sizeof(rawicmp_t) + pkt->size;
+  raw = evalicmp(pkt);
+  if(!raw) {printf("Raw pkt error: 24\n"); exit(1);}
+
+  printf("raw info:\t %x\n", raw);
+  //printf("raw info is %hhn\n", raw);
+  free(pkt->data);
+  free(pkt);
   return 0;
 }
--- ping.h
+++ ping.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define compact __attribute__ ((packed))
 
 
 typedef enum
@@ -17,9 +16,9 @@ typedef enum
 struct icmp_t
 {
   e_type type;
-  int8_t data[];
-
-} compact;
+  int16_t size;
+  int8_t *data;
+} __attribute__ ((packed));
 
 struct rawicmp_t
 {
@@ -27,37 +26,100 @@ struct rawicmp_t
   int8_t code;
   int16_t checksum;
   int8_t data[];
-} compact;
+} __attribute__ ((packed));
 
 typedef struct rawicmp_t rawicmp_t;
 typedef struct icmp_t icmp_t;
 
-rawicmp_t *mkicmp(int8_t type, int8_t code, int8_t *data, int16_t size);
+icmp_t *mkicmp(e_type type, const int8_t *data, int16_t size);
+int8_t *evalicmp(icmp_t*);
+void showicmp(icmp_t*);
+int16_t mk_checksum(int16_t count, void* addr);
+
+
+
 
 
 #if defined(PING_IMPLEMENTATION)
 
 
-//struct rawicmp_t rawwy1 [[gnu::aligned(1)]];
-
-rawicmp_t *mkicmp(int8_t type, int8_t code, int8_t *data, int16_t size)
+icmp_t *mkicmp(e_type type, const int8_t *data, int16_t size)
 {
-  if(!data || !size) return (struct rawicmp_t*)0;
+  if(!data || !size) return (icmp_t*)0;
 
   int16_t n;
-  struct rawicmp_t *p;
+  icmp_t *p;
   n = sizeof(icmp_t) + size;
-  p = (rawicmp_t*)malloc(n);
+  p = (icmp_t*)malloc(n);
   if(!p) exit(1);
   memset(p,0,n);
   p->type = type;
-  p->code = code;
-  memcpy(p->data, data, n);
-  //p->checksum = checksum();
-  //TODO: implement checksum function
+  p->size = size;
+  //memcpy(p->data, data, n);
+  p->data = data;
   return p;
 }
 
+void showicmp(icmp_t *pkt)
+{
+  if(!pkt) return;
+  printf("type:\t %s\nsize:\t %d\npayload data:\t %s\n", (pkt->type == echo) ? "echo" : "echo reply", (int)pkt->size, (char*)pkt->data);
+  return;
+}
+
+int8_t *evalicmp(icmp_t *pkt)
+{
+  if(!pkt || !pkt->data) {printf("No icmp package or empty data\n"); exit(1);}
+  int8_t *p, *ret;
+  int16_t checksum;
+  rawicmp_t rawpkt, *rawptr;
+
+  switch(pkt->type)
+    {
+    case echo:
+      rawpkt.type = echo;
+      rawpkt.code = 0;
+      break;
+    case echoreply:
+      rawpkt.type = echoreply;
+      rawpkt.code = 0;
+      break;
+    default: return (int8_t*)0;
+    }
+  rawpkt.checksum = 0;
+  int16_t s_size  = sizeof(rawicmp_t) + pkt->size;
+  if(!(s_size%2)) ++s_size;
+
+  p = (int8_t*)malloc(s_size);
+  ret = p;
+
+  if(!p) { printf("Memory Allocation issue: 97\n"); exit(1);}
+  memset(p, 0, s_size);
+
+  memcpy(p, (int8_t *)&rawpkt, sizeof(rawicmp_t));
+  p += sizeof(rawicmp_t);
+  memcpy(p, pkt->data, pkt->size);
+
+  checksum = mk_checksum(s_size, ret);
+  rawptr = (rawicmp_t*)ret;
+  rawptr->checksum = checksum;
+
+  return ret;
+
+}
+
+int16_t mk_checksum(int16_t count, void* addr)
+{
+  int16_t *p;
+  int32_t a;
+  a = 0;
+  for(p = (int16_t*)addr; count>0; count-=2, ++p)
+    {
+      a += *p;
+    }
+
+  return ~( ((a & 0xffff0000)>>16) + (a & 0xffff0000) );
+}
 
 
 #endif
